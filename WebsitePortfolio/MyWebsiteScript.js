fb25098167994bf9befadddb5e4a29c0220a8931
ZulfahmiZaid <114953316+ZulfahmiZaid@users.noreply.github.com>
2023-02-16 14:34:08 +0100

    added minHeight and minWidth rules in one line JS

--- WebsitePortfolio/MyWebsiteScript.js
+++ WebsitePortfolio/MyWebsiteScript.js
@@ -43,8 +43,7 @@ let mobile = detector.mobile();
 const body = document.querySelector("body");
 
 if(mobile === null && detector.tablet() === null) {
-   body.style.minHeight = "600px !important;";
-   body.style.minWidth = "600px !important;";
+   body.style.cssText = "min-width: 600px !important; min-height: 600px !important;";
 }
 
 const button_active_color = "white";
